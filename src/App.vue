de714df12f18018786f56dab4aade233765fcac4
Stepankc <i21s692@students.it-college.ru>
2024-01-09 19:41:41 +0500

    feat: add new components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    <MyHeader/>
+    <MyHeader />
+    <item-list :posts="posts" />
+    <MyDrawer />
   </div>
 </template>
 
-<script>
-import MyHeader from './components/MyHeader.vue';
-export default {
-components: {
-  MyHeader
-}
-}
+<script setup>
+import { ref } from 'vue';
+import MyHeader from "./components/MyHeader";
+import ItemList from "./components/ItemList";
+import MyDrawer from "./components/MyDrawer";
+
+const posts = ref([
+  { id: 1, image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg", title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops', body: '109.95' },
+  { id: 2, image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg", title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops', body: 'Описание поста 2' },
+  { id: 3, image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg", title: 'js 3', body: 'Описание поста 3' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 1, title: 'js', body: 'Описание поста 1' },
+  { id: 2, title: 'js 2', body: 'Описание поста 2' },
+  { id: 3, title: 'js 3', body: 'Описание поста 3' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' },
+  { id: 4, title: 'js 4', body: 'Описание поста 4' }
+]);
 </script>
 
 <style lang="scss">
@@ -18,5 +44,6 @@ components: {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font: "Fira Sans", sans-serif;
 }
 </style>
