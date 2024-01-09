66189795a6f4d901288884ead51934922f4f6962
Stepankc <i21s692@students.it-college.ru>
2024-01-09 19:39:23 +0500

    feat: add card and list of items

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <div class="card-img">
+            <img :src="post.image" alt="" class="img">
+        </div>
+        <div class="card-info">
+            <h4 class="title">{{ post.title }}</h4>
+            <div>${{ post.body }}</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    post: Object,
+    required: Boolean,
+})
+
+// props: {
+//         post: Object,
+//         required: Boolean,
+//     },
+</script>
+
+<style lang="scss" scoped>
+.card {
+    margin: 15px;
+    /* border: 2px solid teal; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    row-gap: 10px;
+
+    .img {
+        max-height: 300px;
+    }
+
+    .card-info {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        font-size: large;
+        color: rgb(13, 13, 13);
+
+    }
+}
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <item-card v-for="post in posts" :post="post" />
+    </div>
+</template>
+
+<script lang="ts">
+import ItemCard from './ItemCard.vue';
+export default {
+    components: { ItemCard },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(4, 18%);
+    justify-content: center;
+    gap: 20px;
+}
+</style>
