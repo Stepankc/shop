a0a702b5ba5002c844f3e8378491253ee32597c4
Stepankc <i21s692@students.it-college.ru>
2024-01-09 19:37:58 +0500

    feat: add counter ui

---
+++ src/components/UI/MyCounter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="counter-container">
+        <label for="quantity">Количество:</label>
+        <select id="quantity" v-model="count">
+            <option v-for="quantity in quantityOptions" :key="quantity" :value="quantity">{{ quantity }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const maxQuantity = 10;
+const quantityOptions: number[] = [];
+
+for (let i = 1; i <= maxQuantity; i++) {
+    quantityOptions.push(i);
+}
+
+const count = ref(1);
+</script>
+
+<style lang="scss" scoped>
+.counter-container {
+    display: flex;
+    align-items: center;
+
+    label {
+        margin-right: 10px;
+    }
+
+    select {
+        padding: 8px;
+        font-size: 14px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        outline: none;
+        cursor: pointer;
+
+        &:hover {
+            border-color: #aaa;
+        }
+    }
+}
+</style>
