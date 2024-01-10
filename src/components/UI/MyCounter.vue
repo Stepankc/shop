680bb517144fa364c0669dd8935f9712d27654ae
Stepankc <i21s692@students.it-college.ru>
2024-01-10 14:57:30 +0500

    refactor: change function to array.from

--- src/components/UI/MyCounter.vue
+++ src/components/UI/MyCounter.vue
@@ -1,46 +1,53 @@
 <template>
-    <div class="counter-container">
-        <label for="quantity">Количество:</label>
-        <select id="quantity" v-model="count">
-            <option v-for="quantity in quantityOptions" :key="quantity" :value="quantity">{{ quantity }}
-            </option>
-        </select>
-    </div>
+  <div class="counter-container">
+    <label for="quantity">Количество:</label>
+    <select id="quantity" v-model="count">
+      <option
+        v-for="quantity in quantityOptions"
+        :key="quantity"
+        :value="quantity"
+      >
+        {{ quantity }}
+      </option>
+    </select>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const maxQuantity = 10;
-const quantityOptions: number[] = [];
-
-for (let i = 1; i <= maxQuantity; i++) {
-    quantityOptions.push(i);
-}
-
-const count = ref(1);
+<script lang="ts">
+export default {
+  data() {
+    const quantityOptions: number[] = Array.from(
+      { length: 10 },
+      (_, index) => index + 1
+    );
+    return {
+      count: 1 as number,
+      quantityOptions,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .counter-container {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    label {
-        margin-right: 10px;
+  label {
+    margin-right: 10px;
+  }
+
+  select {
+    padding: 8px;
+    font-size: 14px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    outline: none;
+    cursor: pointer;
+
+    &:hover {
+      border-color: #aaa;
     }
-
-    select {
-        padding: 8px;
-        font-size: 14px;
-        border: 1px solid #ddd;
-        border-radius: 4px;
-        outline: none;
-        cursor: pointer;
-
-        &:hover {
-            border-color: #aaa;
-        }
-    }
+  }
 }
 </style>
