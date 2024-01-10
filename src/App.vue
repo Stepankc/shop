6170d63001509849f98cc41b51cf1c929b4ca6f7
Stepankc <i21s692@students.it-college.ru>
2024-01-10 16:52:48 +0500

    feat: add fetch

--- src/App.vue
+++ src/App.vue
@@ -7,57 +7,28 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
 import MyHeader from "./components/MyHeader.vue";
 import ItemList from "./components/ItemList.vue";
 import MyDrawer from "./components/MyDrawer.vue";
+import { formatPrice } from "./utils/formatPrice";
 
-export default {
+export default defineComponent({
   components: { MyHeader, ItemList, MyDrawer },
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-          title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-          body: "109.95",
-        },
-        {
-          id: 2,
-          image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-          title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-          body: "Описание поста 2",
-        },
-        {
-          id: 3,
-          image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-          title: "js 3",
-          body: "Описание поста 3",
-        },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 1, title: "js", body: "Описание поста 1" },
-        { id: 2, title: "js 2", body: "Описание поста 2" },
-        { id: 3, title: "js 3", body: "Описание поста 3" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-        { id: 4, title: "js 4", body: "Описание поста 4" },
-      ],
+      items: [] as object[],
     };
   },
-};
+  mounted() {
+    fetch(`https://fakestoreapi.com/products/`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.items = data;
+      })
+      .catch((err) => console.log("Error", err));
+  },
+});
 </script>
 
 <style lang="scss">
