4f11e8651812ac92f7eeae54ab97e453ebf49afe
Stepankc <i21s692@students.it-college.ru>
2024-01-13 19:06:44 +0500

    feat: add itemDetails

---
+++ src/components/ItemDetails.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <img class="img" :src="product.image" alt="" />
+      <div class="item-info">
+        <h1>{{ product.title }}</h1>
+        <div>
+          Цена:
+          <strong>{{ currency }}</strong>
+        </div>
+      </div>
+      <div class="decription">
+        <h3>Описание:</h3>
+        <p class="item-decription">{{ product.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+import Product from "./Types";
+import { fetchProducts } from "../api";
+import { formatPrice } from "../utils/formatPrice";
+
+export default {
+  props: {
+    id: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      product: {} as Product,
+    };
+  },
+  computed: {
+    currency() {
+      return formatPrice(this.product.price);
+    },
+  },
+  async mounted() {
+    try {
+      this.product = await fetchProducts(this.id);
+    } catch (error) {
+      console.error("Error fetching product details:", error);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 16px;
+  display: flex;
+  justify-content: center;
+}
+
+.content {
+  max-width: 75%;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 20px;
+}
+
+.img {
+  max-height: 50vh;
+}
+
+.item-info {
+  display: flex;
+  flex-flow: column;
+  gap: 10px;
+  font-size: large;
+}
+
+.decription {
+  grid-column: span 2;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+}
+</style>
