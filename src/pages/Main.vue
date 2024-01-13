4839a064893716a6f227a1f66e4974ddd3536ff1
Stepankc <i21s692@students.it-college.ru>
2024-01-13 19:07:40 +0500

    feat: add new main page

---
+++ src/pages/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <item-list :items="items" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { fetchProducts } from "../api/index";
+import ItemList from "../components/ItemList.vue";
+
+export default defineComponent({
+  components: { ItemList },
+  data() {
+    return {
+      items: [] as object[],
+      cart: [] as object[],
+    };
+  },
+  methods: {
+    addToCart(item: object): void {
+      this.cart.value.push(item);
+    },
+    fetchProducts(): void {
+      fetchProducts().then((data) => {
+        this.items = data;
+      });
+    },
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+});
+</script>
+
+<style lang="scss"></style>
