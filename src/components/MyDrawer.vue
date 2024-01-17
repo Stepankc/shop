28b9ec648b689516ad96e4b504dd4e9d898f9908
Stepankc <i21s692@students.it-college.ru>
2024-01-17 17:25:05 +0500

    fix: router path

--- src/components/MyDrawer.vue
+++ src/components/MyDrawer.vue
@@ -47,7 +47,7 @@ export default {
       this.$emit("toggle-drawer");
     },
     goToCart(): void {
-      this.$router.push("cart");
+      this.$router.push({name: 'Order'});
       this.toggleDrawer();
     },
   },
